d4d582cd4e754ddd93b1d3b4b2bca3a9ff4d6a22
Snir Koppelman <snirkwork@gmail.com>
2023-08-15 20:33:39 +0300

    Add http metrics server
    Refactor server main to start both servers\n and enable gracesful shutdown

--- server/go.mod
+++ server/go.mod
@@ -3,13 +3,18 @@ module github.com/snirkop89/grpc-go-pro/server
 go 1.20
 
 require (
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.0-rc.5
+	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.0-rc.5 // indirect
-	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
-	google.golang.org/grpc v1.57.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
@@ -20,23 +26,69 @@ type server struct {
 
 func main() {
 	args := os.Args[1:]
-	if len(args) == 0 {
-		log.Fatalln("usage: server [IP_ADDR]")
+	if len(args) != 2 {
+		log.Fatalln("usage: server [GRPC_IP_ADDR] [METRICS_IP_ADDR]")
 	}
+	grpcAddr := args[0]
+	httpAddr := args[1]
 
-	addr := args[0]
-	lis, err := net.Listen("tcp", addr)
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v\n", addr)
+		log.Fatalf("failed to listen: %v\n", grpcAddr)
 	}
-	defer func(list net.Listener) {
-		if err := lis.Close(); err != nil {
-			log.Fatalf("unexpected error: %v", err)
+	defer lis.Close()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	grpcSrv, err := newGrpcServer(lis)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.Go(func() error {
+		log.Printf("gRPC server listening at %s\n", grpcAddr)
+		if err := grpcSrv.Serve(lis); err != nil {
+			log.Printf("failed starting gRPC server: %v\n", err)
+			return err
 		}
-	}(lis)
+		log.Println("gRPC server shutdown")
+		return nil
+	})
+	metricsServer := newMetricsServer(httpAddr)
+	g.Go(func() error {
+		log.Printf("metrics server listening at %s\n", httpAddr)
+		if err := metricsServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to serve metrics: %v\n", err)
+			return err
+		}
+		log.Println("metrics server shutdown")
+		return nil
+	})
 
-	log.Printf("listening at %s\n", addr)
+	<-ctx.Done()
+	cancel()
+	timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer timeoutCancel()
+	log.Println("Shutting down server, please wait...")
+	grpcSrv.GracefulStop()
+	metricsServer.Shutdown(timeoutCtx)
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
 
+func newMetricsServer(httpAddr string) *http.Server {
+	m := http.NewServeMux()
+	return &http.Server{
+		Addr:    httpAddr,
+		Handler: m,
+	}
+}
+
+func newGrpcServer(lis net.Listener) (*grpc.Server, error) {
 	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime)
 
 	creds, err := credentials.NewServerTLSFromFile("./certs/server_cert.pem", "./certs/server_key.pem")
@@ -57,9 +109,5 @@ func main() {
 	}
 	s := grpc.NewServer(opts...)
 	pb.RegisterTodoServiceServer(s, &server{d: New()})
-	defer s.Stop()
-	// registration of endpoints
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v\n", err)
-	}
+	return s, nil
 }
